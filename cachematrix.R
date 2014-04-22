8e4d0ada46e8b181a54b414ba626ed303ec976b0
Natapone C <natapone@gmail.com>
2014-04-22 16:40:21 +0700

    move genInvMatrix to main file

--- cachematrix.R
+++ cachematrix.R
@@ -68,3 +68,11 @@ cachemean <- function(x, ...) {
     x$setmean(m)
     m
 }
+
+# Generate random number for Invertible matrix
+genInvMatrix <- function(size = 100, seed=123) {
+    set.seed(seed)
+    m <- matrix(rnorm(size * size, mean=10, sd=0.5), nrow=size, ncol=size)
+    m <- abs(cor(m))
+    m
+}
