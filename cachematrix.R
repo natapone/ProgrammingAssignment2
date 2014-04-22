2c4f807152ad148c8d4ea90be65e4eae091d8576
Natapone C <natapone@gmail.com>
2014-04-22 16:31:18 +0700

    solve and save to cache

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,38 @@
 # A list contains funtion to
 # - get/set the value of matrix
 makeCacheMatrix <- function(x = matrix()) {
+    s <- NULL
     set <- function(y) {
         x <<- y
+        s <- NULL
     }
     get <- function() x
+    
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    
     list (
         set = set, 
-        get = get
+        get = get,
+        setsolve = setsolve,
+        getsolve = getsolve
     )
 }
 
-## Write a short comment describing this function
-
+## Solve Inverse matrix and store in cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    
+    ## Return a matrix that is the inverse of 'x'
+    x$setsolve(s)
+    s
+    
 }
 
 # A list contains funtion to
